fd034930e25c849407194ba4d2d67e77762101a5
nomy18-meet <neom21leva@gmail.com>
2017-01-08 19:49:11 +0200

    another project

--- my_str.py
+++ my_str.py
@@ -1,5 +1,5 @@
 #FIX THE LINE BELOW
-class xyz : #<-----Replace xyz-make a new class, MyStr, that inherits from str
+class MyStr (str) : #<-----Replace xyz-make a new class, MyStr, that inherits from str
     """
     Build a subclass of str with some new, fun methods.
     """
@@ -42,4 +42,6 @@ class xyz : #<-----Replace xyz-make a new class, MyStr, that inherits from str
         #       do this in as little as 1 line.
         #YOUR CODE BELOW:
         #################
+        return super (MyStr, self).replace(take_out,use_this)
+    
         
